df67856bb9beecee2401123c7fbd6523658127b6
Will579440 <willbridges454@gmail.com>
2020-03-30 18:06:38 +0100

    Added functionality to Cell class, e.g. the member variables and constructor + destructor.

--- Sudoku/Sudoku/Cell.h
+++ Sudoku/Sudoku/Cell.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <vector>
+
+using namespace std;
+
 class Cell
 {
+public:
+	~Cell(void); // Destructor.
+	Cell(void); // Void constructor.
+
+
+private:
+	vector<int> m_candidateList; // The candidate list (using vector as it's easier) i.e. the possible cell values.
+	int m_value; // The contained value of the Cell, if known.
+	bool m_givenFlag; // The flag to show whether the value was given, or found out later.
 };
 
