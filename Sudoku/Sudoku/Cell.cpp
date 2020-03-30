f8c46d2fb9a31004e9830977a27d5f0db65d3f74
Will579440 <willbridges454@gmail.com>
2020-03-30 19:29:54 +0100

    Added most/ all functionality needed at the moment, to the Cell class.

--- Sudoku/Sudoku/Cell.cpp
+++ Sudoku/Sudoku/Cell.cpp
@@ -1,10 +1,42 @@
 #include "Cell.h"
 
+Cell::Cell(const int value, const bool givenFlag) // Constructor with params, to populate a cell.
+{
+	// Filling up the candidate list, from 1 to 9.
+	for (int num = 1; num < 10; num++)
+	{
+		m_candidateList.push_back(num);
+	}
+
+	m_value = value;
+	m_givenFlag = givenFlag;
+}
+
 Cell::~Cell(void)
 {
 }
 
-Cell::Cell(void)
+Cell::Cell(void) // Default constructor, initialising empty cells.
 {
+	// Filling up the candidate list, from 1 to 9.
+	for (int num = 1; num < 10; num++)
+	{
+		m_candidateList.push_back(num);
+	}
 
+	m_value = 0;
+	m_givenFlag = false;
 }
+
+/*void Cell::removeCandidateValue(const int value)
+{
+	// Iterating over a vector properly.
+	// Found at: https://stackoverflow.com/questions/409348/iteration-over-stdvector-unsigned-vs-signed-index-variable
+	for (std::vector<int>::iterator it = m_candidateList.begin(); it != m_candidateList.end(); ++it) 
+	{
+		if (m_candidateList[it] == value)
+		{
+
+		}
+	}
+}*/
--- Sudoku/Sudoku/Cell.h
+++ Sudoku/Sudoku/Cell.h
@@ -6,8 +6,42 @@ using namespace std;
 class Cell
 {
 public:
+	Cell(const int value, const bool givenFlag); // Main, non-default constructor.
+
 	~Cell(void); // Destructor.
 	Cell(void); // Void constructor.
+
+	// Getters/ accessors for member variables, candidate values and list size.
+	// Inlined for performance.
+	inline int getValue() const
+	{
+		return m_value;
+	}
+	inline bool getGivenFlag() const
+	{
+		return m_givenFlag;
+	}
+	inline int getCandidateValueAtIndex(const int index) const
+	{
+		return m_candidateList[index];
+	}
+	inline int getCandidateListSize() const
+	{
+		return m_candidateList.size();
+	}
+
+	// Setters/ mutators for member variables.
+	inline void setValue(const int value)
+	{
+		m_value = value;
+	}
+	inline void setGivenFlag(const bool flagState)
+	{
+		m_givenFlag = flagState;
+	}
+
+	// Methods to alter the candidate list.
+	//void removeCandidateValue(const int value);
 
 
 private:
