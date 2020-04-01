d3d462fb6e24a6dd836998f1ea6afaa91c628a99
Will579440 <willbridges454@gmail.com>
2020-04-01 15:54:32 +0100

    Implemented some CellGroup.h functionality. Added void constructor functionality into CellGroup.cpp file.

--- Sudoku/Sudoku/CellGroup.cpp
+++ Sudoku/Sudoku/CellGroup.cpp
@@ -1,1 +1,13 @@
 #include "CellGroup.h"
+
+CellGroup::CellGroup(void) // To initialise CellGroup state.
+{
+	for (int index = 0; index < 9; ++index)
+	{
+		m_cells[index] = new Cell();
+	}
+}
+
+CellGroup::~CellGroup(void)
+{ }
+
--- Sudoku/Sudoku/CellGroup.h
+++ Sudoku/Sudoku/CellGroup.h
@@ -1,5 +1,19 @@
 #pragma once
+#include "Cell.h"
+
 class CellGroup
 {
+public:
+	CellGroup(void);
+	~CellGroup(void);
+
+	// Getter/ accessor for a cell (return a pointer).
+	Cell* getCell(const int index) const;
+
+	// Setter/ mutator for a cell (via a pointer).
+	void setCell(const int index, const Cell *cell);
+
+private:
+	Cell *m_cells[9];
 };
 
