b28e381a40df20a41e268c912d0be402da7ce960
Will579440 <willbridges454@gmail.com>
2020-04-02 00:53:49 +0100

    Added functionality to copy pointers from the CellGroup gridRows[] into the CellGroup gridColumns[].

--- Sudoku/Sudoku/SudokuPuzzle.cpp
+++ Sudoku/Sudoku/SudokuPuzzle.cpp
@@ -40,11 +40,11 @@ void SudokuPuzzle::readPuzzle(const char filenameIn[])
 	// First part of algorithm needs to input all the file values into the CellGroup for gridRows[9], as specified in ACW.
 
 	int inputValue; // Declare these ONCE in the entire class, if it's re-used in future code (performance increase).
-	bool givenFlag;
 	for (int row = 0; row < m_puzzleSize; row++) // Leaving as post++, as doesn't affect performance with ints.
 	{
 		for (int column = 0; column < m_puzzleSize; column++)
 		{
+			bool givenFlag = true;
 			fin >> inputValue;
 
 			if (inputValue == 0)
@@ -52,7 +52,16 @@ void SudokuPuzzle::readPuzzle(const char filenameIn[])
 				givenFlag = false;
 			}
 
+			// Reading file values into the CellGroup for the rows.
 			m_gridRows[row].setCell(column, new Cell(inputValue, givenFlag));
+
+			// Using the Rows CellGroup to populate the Column CellGroup. To setCell from the Rows CellGroup, the setCell index param has to 
+			// be the row, and the Cell we want to point to has to be in the current row we're on in the loop. Then, we getCell on the correct 
+			// element index in the row. This gives us the correct cell to be pointed to by the correct element in our Column CellGroup.
+			m_gridColumns[column].setCell(row, m_gridRows[row].getCell(column));
+
+			// Using the Rows CellGroup to populate the Block CellGroup.
+			m_gridBlocks[row].setCell(row, m_gridRows[row].getCell(column)); // NOT WORKING. Gonna be complicated...
 		}
 	}
 }
